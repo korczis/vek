3118480d7b363f872dfec6c7e399943741cb3813
Luiz Berti <luizberti@users.noreply.github.com>
2018-07-10 23:06:29 -0300

    nicer vector primitives

--- src/vecs.rs
+++ src/vecs.rs
@@ -22,8 +22,15 @@ pub struct DenseVector {
 
 pub trait Distances {
     fn norm(&self) -> f32;
-    fn cosine(&self, other: &Self) -> f32;  // assumes vectors are L2-normalised
+
+    // fn mae(&self, other: &Self, n: usize) -> f32;
+    fn sad(&self, other: &Self) -> f32;
+
+    // fn mse(&self, other: &Self, n: usize) -> f32;
     fn euclidean(&self, other: &Self) -> f32;
+    fn ssd(&self, other: &Self) -> f32;
+
+    fn cosine(&self, other: &Self) -> f32;  // assumes vectors are L2-normalised
 }
 
 
@@ -36,6 +43,51 @@ pub trait Transformations {
 
 impl Distances for SparseVector {
     fn norm(&self) -> f32 { self.value.iter().map(|value| value.powi(2)).sum::<f32>().sqrt() }
+
+    // fn mae(&self, other: &SparseVector, n: usize) -> f32 { self.sad(other) / (n as f32) }
+    fn sad(&self, other: &SparseVector) -> f32 {
+        let (mut accumulator, mut refiter, mut cmpiter) = (0f32, 0usize, 0usize);
+
+        while refiter < self.index.len() && cmpiter < other.index.len() {
+            let (refidx, refval) = (self.index[refiter], self.value[refiter]);
+            let (cmpidx, cmpval) = (other.index[cmpiter], other.value[cmpiter]);
+
+            match refidx.cmp(&cmpidx) {
+                Ordering::Equal => {
+                    accumulator += (refval - cmpval).abs();
+                    refiter += 1;
+                    cmpiter += 1;
+                },
+                Ordering::Less => refiter += 1,
+                Ordering::Greater => cmpiter += 1,
+            };
+        }
+
+        accumulator
+    }
+
+    // fn mse(&self, other: &SparseVector, n: usize) -> f32 { self.ssd(other) / (n as f32) }
+    fn euclidean(&self, other: &SparseVector) -> f32 { self.ssd(other).sqrt() }
+    fn ssd(&self, other: &SparseVector) -> f32 {
+        let (mut accumulator, mut refiter, mut cmpiter) = (0f32, 0usize, 0usize);
+
+        while refiter < self.index.len() && cmpiter < other.index.len() {
+            let (refidx, refval) = (self.index[refiter], self.value[refiter]);
+            let (cmpidx, cmpval) = (other.index[cmpiter], other.value[cmpiter]);
+
+            match refidx.cmp(&cmpidx) {
+                Ordering::Equal => {
+                    accumulator += (refval - cmpval).powi(2);
+                    refiter += 1;
+                    cmpiter += 1;
+                },
+                Ordering::Less => refiter += 1,
+                Ordering::Greater => cmpiter += 1,
+            };
+        }
+
+        accumulator
+    }
 
     fn cosine(&self, other: &SparseVector) -> f32 {
         let (mut accumulator, mut refiter, mut cmpiter) = (0f32, 0usize, 0usize);
@@ -57,42 +109,27 @@ impl Distances for SparseVector {
 
         accumulator
     }
-
-    fn euclidean(&self, other: &SparseVector) -> f32 {
-        let (mut accumulator, mut refiter, mut cmpiter) = (0f32, 0usize, 0usize);
-
-        while refiter < self.index.len() && cmpiter < other.index.len() {
-            let (refidx, refval) = (self.index[refiter], self.value[refiter]);
-            let (cmpidx, cmpval) = (other.index[cmpiter], other.value[cmpiter]);
-
-            match refidx.cmp(&cmpidx) {
-                Ordering::Equal => {
-                    accumulator += (refval - cmpval).powi(2);
-                    refiter += 1;
-                    cmpiter += 1;
-                },
-                Ordering::Less => refiter += 1,
-                Ordering::Greater => cmpiter += 1,
-            };
-        }
-
-        accumulator.sqrt()
-    }
 }
 
 
 impl Distances for DenseVector {
     fn norm(&self) -> f32 { self.value.iter().map(|value| value.powi(2)).sum::<f32>().sqrt() }
 
-    fn cosine(&self, other: &DenseVector) -> f32 {
+    fn sad(&self, other: &DenseVector) -> f32 {
         self.value.iter().zip(other.value.iter())
-        .fold(0f32, |accumulator, (reference, comparison)| reference.mul_add(*comparison, accumulator))
+        .map(|(reference, comparison)| (reference - comparison).abs())
+        .sum::<f32>()
     }
 
-    fn euclidean(&self, other: &DenseVector) -> f32 {
+    fn euclidean(&self, other: &Self) -> f32{ self.ssd(other).sqrt() }
+    fn ssd(&self, other: &DenseVector) -> f32 {
         self.value.iter().zip(other.value.iter())
         .map(|(reference, comparison)| (reference - comparison).powi(2))
         .sum::<f32>()
-        .sqrt()
+    }
+
+    fn cosine(&self, other: &DenseVector) -> f32 {
+        self.value.iter().zip(other.value.iter())
+        .fold(0f32, |accumulator, (reference, comparison)| reference.mul_add(*comparison, accumulator))
     }
 }
